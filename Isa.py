48355cc1ab6c4458a693c8fb4cce4a718b6f68c6
ManishTanganiya <101615173+ManishTanganiya@users.noreply.github.com>
2022-03-23 10:42:20 +0530

    Add files via upload

---
+++ Isa.py
@@ -0,0 +1,71 @@
+#Name: Tanganiya Manishsingh Lalitsingh ROll no:65
+def aStarAlgo(start_node, stop_node):       
+        open_set = set(start_node) 
+        closed_set = set()
+        g = {} 
+        parents = {}        
+        g[start_node] = 0
+        parents[start_node] = start_node                 
+        while len(open_set) > 0:
+            n = None            
+            for v in open_set:
+                if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
+                    n = v                                  
+            if n == stop_node or Graph_nodes[n] == None:
+                pass
+            else:
+                for (m, weight) in get_neighbors(n):
+                    if m not in open_set and m not in closed_set:
+                        open_set.add(m)
+                        parents[m] = n
+                        g[m] = g[n] + weight
+                    else:
+                        if g[m] > g[n] + weight:
+                            g[m] = g[n] + weight
+                            if m in closed_set:
+                                closed_set.remove(m)
+                                open_set.add(m) 
+            if n == None:
+                print('Path does not exist!')
+                return None
+            if n == stop_node:
+                path = []
+                while parents[n] != n:
+                    path.append(n)
+                    n = parents[n] 
+                path.append(start_node)
+                path.reverse() 
+                print('Path found: {}'.format(path))
+                return path
+            open_set.remove(n)
+            closed_set.add(n) 
+        print('Path does not exist!')
+        return None
+def get_neighbors(v):
+    if v in Graph_nodes:
+        return Graph_nodes[v]
+    else:
+        return None
+def heuristic(n):
+        H_dist = {
+            'S': 6,
+            'A': 8,
+            'B': 6,
+            'C': 5,
+            'D': 4,
+            'E': 2,
+            'F': 1,
+            'G': 0,            
+        }
+        return H_dist[n]
+Graph_nodes = {
+    'S': [('A', 6),('B',2),('C', 1)],
+    'A': [('D', 3),('G', 20)],
+    'B': [('D', 2),('E',6)],
+    'C': [('F', 4),('E',6)],
+    'D': [('F', 5)],
+    'E': [('G', 2)],
+    'F': [('G', 1)],
+    'G': None,    
+}
+aStarAlgo('S', 'G')
